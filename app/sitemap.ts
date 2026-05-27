0de7337bd49ab7e33fd701109112cedd9599c28b
Oetewe <oetewe2021@gmail.com>
2026-05-27 13:10:38 +0700

    add sitemap

---
+++ app/sitemap.ts
@@ -0,0 +1,24 @@
+export default function sitemap() {
+  return [
+    {
+      url: "https://oetewe2021.vercel.app",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://oetewe2021.vercel.app/artikel",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://oetewe2021.vercel.app/kegiatan",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://oetewe2021.vercel.app/galeri",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://oetewe2021.vercel.app/tentang",
+      lastModified: new Date(),
+    },
+  ]
+}
